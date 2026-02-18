38231e8e63eb7842b132a1bc8190a0d266a3b7c2
Cayden <cha24075@byui.edu>
2026-02-18 15:35:50 -0700

    lesson 17

---
+++ Lsn17-greeting.py
@@ -0,0 +1,5 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+favorite_color = input("Enter your favorite color: ")
+
+print(f"Hello there {first_name} {last_name}! Your favorite color is {favorite_color}.")
